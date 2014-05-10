8b1bd747d8e01646ae9362d5a72cde7786e649eb
sandeep <sandeep.sreenath@gmail.com>
2014-05-10 19:22:19 +0530

    map with multiple markers

--- client/scripts/controllers/projects-map-controller.js
+++ client/scripts/controllers/projects-map-controller.js
@@ -4,9 +4,26 @@ angular.module('myApp').controller('ProjectsMapController',
   $scope.projects = Projects.query();
   $scope.map = {
     center: {
-        latitude: 45,
-        longitude: -73
+        latitude: 12,
+        longitude: 78
     },
-    zoom: 8
+    zoom: 8,
+    draggable: "true"
   };
+
+
+
+  $scope.markers = [
+    {
+      latitude: 12.5,
+      longitude: 78.5
+    },
+    {
+      latitude: 12.8,
+      longitude: 78.2
+    }
+  ];
+
+  $scope.fit = true;
+
 });

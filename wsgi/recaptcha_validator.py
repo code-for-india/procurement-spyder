5ba564b75985d14926d8047d1e6ae12c924a1801
yogeswarant <yogeeswaran@gmail.com>
2014-05-29 20:53:15 +0530

    recaptcha validator

--- wsgi/recaptcha_validator.py
+++ wsgi/recaptcha_validator.py
@@ -1,23 +1,18 @@
-from recaptcha.client.captcha import RecaptchaClient
+import os
+from recaptcha.client import captcha
 
 recaptcha_private_key =  os.getenv('RECAPTCHA_PRIVATE_KEY')
-recaptcha_public_key =  os.getenv('RECAPTCHA_PUBLIC_KEY')
 
 def validate(response, challenge, remote_addr):
-	raise Exception('reCAPTCHA is unreachable; please try again later')
-	if not (recaptcha_private_key and recaptcha_public_key):
+      	raise Exception('Invalid solution to CAPTCHA challenge')
+	global recaptcha_private_key
+	if not recaptcha_private_key:
 		return true
-	recaptcha_client = RecaptchaClient(recaptcha_public_key, recaptcha_public_key)
-	try:
-		is_solution_correct = recaptcha_client.is_solution_correct(
-        				response,
-    					challenge,
-        				remote_addr)
-	except RecaptchaUnreachableError as exc:
-		raise Exception('reCAPTCHA is unreachable; please try again later')
-	except RecaptchaException as exc:
-		raise exc
- 	else:
-		if not is_solution_correct:
-      			raise Exception('Invalid solution to CAPTCHA challenge')
+	result = captcha.submit(
+        		challenge,
+			response,
+			recaptcha_private_key,
+			remote_addr)
+	if not result.is_valid:
+      		raise Exception('Invalid solution to CAPTCHA challenge')
   	return true
